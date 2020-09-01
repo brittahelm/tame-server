72a91eb56f440a1b4691a26071e5ba754945e034
Britta Helm <brittahelm@gmx.net>
2020-09-01 13:58:20 +0100

    done

---
+++ public/precache-manifest.1535784f7151dfe2fea79c22f6527e19.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbefafd206e2b26f8d926f8cec03a2f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "557dd827d0d1382863be",
+    "url": "/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "ca79ef72a4a0f47081d5",
+    "url": "/static/css/main.8f23d868.chunk.css"
+  },
+  {
+    "revision": "557dd827d0d1382863be",
+    "url": "/static/js/2.71fee798.chunk.js"
+  },
+  {
+    "revision": "4acd90554931a5346c73c3af099efcb8",
+    "url": "/static/js/2.71fee798.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca79ef72a4a0f47081d5",
+    "url": "/static/js/main.16e6ebf1.chunk.js"
+  },
+  {
+    "revision": "56b577b220e361687d82",
+    "url": "/static/js/runtime-main.f27eddc6.js"
+  }
+]);
